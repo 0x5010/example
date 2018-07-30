99e3e551663133c4a93b89842421c3b68544a910
0x5010 <0xh4cker@gmail.com>
2018-07-30 19:06:59 +0800

    json

--- json/unmarshal/main.go
+++ json/unmarshal/main.go
@@ -11,6 +11,15 @@ type Envelope struct {
 	Msg  interface{} `json:"msg"`
 }
 
+type Sound struct {
+	Description string `json:"description"`
+	Authority   string `json:"authority"`
+}
+
+type Cowbell struct {
+	More bool `json:"more"`
+}
+
 func main() {
 	input := `
 {
@@ -21,12 +30,35 @@ func main() {
     }
 }
 `
-	var env Envelope
-	if err := json.Unmarshal([]byte(input), &env); err != nil {
-		log.Fatal(err)
+	{
+		var env Envelope
+		if err := json.Unmarshal([]byte(input), &env); err != nil {
+			log.Fatal(err)
+		}
+
+		desc := env.Msg.(map[string]interface{})["description"].(string)
+		fmt.Println(desc)
 	}
-	// for the love of Gopher DO NOT DO THIS
-	desc := env.Msg.(map[string]interface{})["description"].(string)
-	fmt.Println(desc)
-	// dynamite
+
+	{
+		var msg json.RawMessage
+		env := Envelope{
+			Msg: &msg,
+		}
+		if err := json.Unmarshal([]byte(input), &env); err != nil {
+			log.Fatal(err)
+		}
+		switch env.Type {
+		case "sound":
+			var s Sound
+			if err := json.Unmarshal(msg, &s); err != nil {
+				log.Fatal(err)
+			}
+			desc := s.Description
+			fmt.Println(desc)
+		default:
+			log.Fatalf("unknown message type: %q", env.Type)
+		}
+	}
+
 }
